5fb5a81edf1d3316a8f821a93f3d47834c68a40c
Erin Stansbury <ekstansbury@gmail.com>
2020-11-03 05:27:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,9 +12,7 @@ class App < Sinatra::Base
   end
   
   post '/puppy' do
-    @name = params['name']
-    @age = params['age']
-    @breed = params['breed']
+    @puppy = Puppy.new(params[:name], params[:breed], params[:age])
     erb :display_puppy
   end
 end
